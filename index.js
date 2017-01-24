66a435d8f45dde6ef89f3c830f48ef76f0ae73d2
Bojan Djurkovic <dbojan@gmail.com>
2017-01-23 22:19:30 -0400

    initial implementation

---
+++ index.js
@@ -0,0 +1,50 @@
+const bearer = require('mali-bearer')
+const pify = require('pify')
+const JWT = pify(require('jsonwebtoken'))
+const get = require('lodash.get')
+const set = require('lodash.set')
+
+function jwt (options) {
+  const opts = options || {}
+  opts.key = opts.key || 'user'
+  opts.tokenKey = opts.tokenKey || 'token'
+  opts.secretPath = opts.secretPath || 'secret'
+
+  const identity = user => user
+  const isRevoked = opts.isRevoked
+    ? (ctx, token) => user => opts.isRevoked(ctx, user, token).then(revocationHandler(user))
+    : () => identity
+
+  function jwtMiddleware (ctx, next) {
+    bearer(opts, (token, ctx, next) => {
+      const secret = get(ctx, opts.secretPath, opts.secret)
+      if (!secret) {
+        throw new Error('Invalid secret')
+      }
+
+      return JWT.verifyAsync(token, secret, opts)
+        .then(isRevoked(ctx, token))
+        .then(user => {
+          set(ctx, opts.key, user)
+          set(ctx, opts.tokenKey, token)
+        })
+        .catch(e => {
+          const msg = 'Invalid token' + (opts.debug
+            ? ' - ' + e.message
+            : '') + '\n'
+          throw new Error(msg)
+        })
+        .then(next)
+    })
+  }
+
+  return jwtMiddleware
+}
+
+function revocationHandler (user) {
+  return revoked => revoked
+    ? Promise.reject(new Error('Revoked token'))
+    : Promise.resolve(user)
+}
+
+module.exports = jwt
